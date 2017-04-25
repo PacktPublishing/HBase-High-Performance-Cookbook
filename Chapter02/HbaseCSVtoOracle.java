1c38a2db94ff5fa647a8cc9a6e8b0b80f18847aa
r0choud <r0choud@172.28.91.42>
2017-04-25 00:47:00 -0700

    Added an Interface and moved all the static resources to it.

--- Chapter02/HbaseCSVtoOracle.java
+++ Chapter02/HbaseCSVtoOracle.java
@@ -7,12 +7,12 @@ import java.sql.SQLException;
 import au.com.bytecode.opencsv.CSVReader;
 // importing the class which will be used to read the CSV file
 
-public class HbaseCSVtoOracle {
+public class HbaseCSVtoOracle implements HbaseCSVConstantsIntf {
 
   public HbaseCSVtoOracle() {
     // TODO Auto-generated constructor stub
   }
-  static Connection conn = null;
+  static Connection conn = null;// Initializing
   static PreparedStatement sql_statement = null; // Initializing
   /**
    * @author r0choud
@@ -21,9 +21,8 @@ public class HbaseCSVtoOracle {
   public static void main(String[] args) {
    
     try{
-    final String inputCSVFile = "/u/HbaseB/WDI_Country.csv"; //getting the CSV file from the location we have saved.
-    Class.forName ("oracle.jdbc.OracleDriver"); // Creating a connection Object
-    conn=DriverManager.getConnection("jdbc:oracle:thin:@//myoracle.db.com:1521/xe", "scott", "tiger");
+    Class.forName (ORACLE_DRIVER_MANAGER); // Creating a connection Object
+    conn=DriverManager.getConnection(THIN_DRIVER_HOST_PORT, ORCALE_USER, ORCALE_PASSWORD);
     /*
      * using s oracle thin driver connecting to the host "your host"
      * using the port as 1512
@@ -35,20 +34,11 @@ public class HbaseCSVtoOracle {
      * getting the data from the CSV file
      * this can be f
      * */
-    final String jdbc_insert_sql = "INSERT INTO WDI_COUNTRY" +"( "
-                    + "(COUNTRY_CODE,SHORT,TABLE_NAME,ALPHA_CODE,CURRENCY_UNIT,SPECIAL_NOTES,REGION,)"
-                    + "(INCOME_GROUP,INTERNATIONAL_MEMBERSHIPS,WB_2_CODE,NATIONAL_ACCOUNTS_BASE_YEAR,)"
-                    + "(NATIONAL_ACCOUNTS_REF_YEAR,SNA_PRICE_VALUATION,LENDING_CATEGORY,OTHER_GROUPS,)"
-                    + "(SYSTEM_OF_NATIONAL_ACCOUNTS,ALTERNATIVE_CONVERSION_FACTOR,PPP_SURVEY_YEAR_BALANCE,)"
-                    + "(EXTERNAL_DEBT_REPORT_STATUS,SYSTEM_OF_TRADE,GOVERNMENT_ACCT_CONCEPT,IMF_DATA_DISSEMINATION_STD,)"
-                    + "(LATEST_POPULATION_CENSUS,LATEST_HOUSEHOLD_SURVEY,SOURCE_OF_MOST_RECENT_INCOME,VITAL_REGISTRATION_COMPLETE,)"
-                    + "(LATEST_AGRICULTURAL_CENSUS,LATEST_INDUSTRIAL_DATA.LATEST_TRADE_DATA,LATEST_WATER_WITHDRAWAL_DATA,)"+")"
-                    + "VALUES"
-                    + "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-    sql_statement = conn.prepareStatement(jdbc_insert_sql); // passing sql query string to the preparedStatement.
+   
+    sql_statement = conn.prepareStatement(JDBC_INSERT_SQL); // passing sql query string to the preparedStatement.
     /* Read CSV file in OpenCSV */
     @SuppressWarnings("resource")
-    CSVReader reader = new CSVReader(new FileReader(inputCSVFile));
+    CSVReader reader = new CSVReader(new FileReader(INPUT_CSVFILE_LOCATON));
     /* Variables to loop through the CSV File */
     String [] nextLine; /* for every line in the file */            
     @SuppressWarnings("unused")
